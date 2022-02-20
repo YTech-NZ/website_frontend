0dcbc9c7d03283113a1417d1ce028a3916028067
johnchen383 <john.bm.chen@gmail.com>
2022-02-20 13:05:17 +1300

    feat: changing capitalisation + fixing types import for react-router-dom

---
+++ src/screens/Landing.tsx
@@ -0,0 +1,13 @@
+{/* import React from 'react' */}
+import NavBar from '../components/NavBar'
+
+function Landing() {
+    return (
+        <>
+            <NavBar />
+            <div>landing</div>
+        </>
+    )
+}
+
+export default Landing
