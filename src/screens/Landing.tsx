47e164d4632d92a62348fc0b063acf9f09efa2c8
johnchen383 <john.bm.chen@gmail.com>
2022-02-23 11:48:35 +1300

    fix: type dependency error

--- src/screens/Landing.tsx
+++ src/screens/Landing.tsx
@@ -1,7 +1,7 @@
 import NavBar from '../components/Navbar';
 import Splash from '../components/Splash';
 import Section from '../widgets/Section';
-import ReactVideo from 'react.video';
+import Video from 'react-video';
 
 function Landing() {
     return (
@@ -10,15 +10,16 @@ function Landing() {
             <Splash />
             <Section>
                 <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti, numquam magni tempore sequi sed nisi nihil vero eum ipsum eligendi autem nulla dicta est. Pariatur optio quas impedit voluptates eveniet!</div>
-            
-                <ReactVideo
-        ref={'VideoComp'}
-        cls={'custom-video'} 
-        height={500} width={'100%'}
-        
-        src={'http://www.html5rocks.com/en/tutorials/video/basics/devstories.mp4'}
-        >
-      </ReactVideo>
+
+                <Video
+                    ref={'VideoComp'}
+                    cls={'custom-video'}
+                    height={500} width={'100%'}
+
+                    src={'http://www.html5rocks.com/en/tutorials/video/basics/devstories.mp4'}
+                >
+                </Video>
+                
             </Section>
             <Section inverse>
                 <div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti, numquam magni tempore sequi sed nisi nihil vero eum ipsum eligendi autem nulla dicta est. Pariatur optio quas impedit voluptates eveniet!</div>
@@ -28,8 +29,8 @@ function Landing() {
                 {/* Source: https://stackoverflow.com/questions/15557392/how-do-i-display-images-from-google-drive-on-a-website  */}
                 {/* Format: https://drive.google.com/uc?export=view&id=... */}
                 {/* Will need to get the id. The id can be found between /d/ and /view  in the original URL*/}
-                <img src="https://drive.google.com/uc?export=view&id=1GD4DUn3ehKOqwz32R-tMjGnWSZw6wVlG"/>
-            
+                <img src="https://drive.google.com/uc?export=view&id=1GD4DUn3ehKOqwz32R-tMjGnWSZw6wVlG" />
+
             </Section>
             <Section inverse>
                 <div>Footer stuff</div>
