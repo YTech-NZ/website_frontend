7c5d040cad6e2d822a4c758cb6b7a9ecf701ee3f
Tia Durovich <durovich.t@maristcollege.school.nz>
2022-04-25 12:03:20 +1200

    Refactor: Cleaned footer SCSS, changed social media linking & icon size

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,29 +9,34 @@ import EMAIL from "../assets/email.png";
 
 
 function Footer() {
+    const LINKEDINURL = "https://www.linkedin.com/company/ytechnz";
+    const FACEBOOKURL = "https://www.facebook.com/ytechnz";
+    const INSTAGRAMURL = "https://www.instagram.com/ytechnz/";
+    const EMAILMAILTO = "mailto:ytech.committee@gmail.com";
+
     return (
         <Section black>
             <div className="footer">
                 <div className="footer__container">
                     <img src={LOGO_BLACK} alt="YTech NZ logo black"/>
                     <div className="footer__container__row">
-                        <div className='linkedin'>
-                            <a href='https://www.linkedin.com/company/ytechnz' target="_blank">
+                        <div>
+                            <a href={LINKEDINURL} target="_blank">
                                 <img src={LINKEDIN} alt="LinkedIn logo"/>
                             </a>
                         </div>
-                        <div className='facebook'>
-                            <a href='https://www.facebook.com/ytechnz' target="_blank"> 
+                        <div>
+                            <a href={FACEBOOKURL} target="_blank"> 
                                 <img src={FACEBOOK} alt="Facebook logo"/>
                             </a>
                         </div>
-                        <div className='instagram'>
-                            <a href='https://www.instagram.com/ytechnz/' target="_blank"> 
+                        <div>
+                            <a href={INSTAGRAMURL} target="_blank"> 
                                 <img src={INSTAGRAM} alt="Instagram logo"/>
                             </a>
                         </div>
-                        <div className='email'>
-                            <a href="mailto:ytech.committee@gmail.com">
+                        <div>
+                            <a href={EMAILMAILTO}>
                                 <img src={EMAIL} alt="Email logo"/>
                             </a>
                         </div>
