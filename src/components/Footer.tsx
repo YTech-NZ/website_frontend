07d4faf2eb97d643efb074c0465b6bd7178541d6
Tia Durovich <durovich.t@maristcollege.school.nz>
2022-04-25 20:19:14 +1200

    Refactor: Properties file

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,15 +5,11 @@ import LINKEDIN from "../assets/linkedin.png";
 import FACEBOOK from "../assets/facebook.png";
 import INSTAGRAM from "../assets/instagram.png";
 import EMAIL from "../assets/email.png";
+import Properties from '../data/Properties';
 
 
 
 function Footer() {
-    const LINKEDINURL = "https://www.linkedin.com/company/ytechnz";
-    const FACEBOOKURL = "https://www.facebook.com/ytechnz";
-    const INSTAGRAMURL = "https://www.instagram.com/ytechnz/";
-    const EMAILMAILTO = "mailto:ytech.committee@gmail.com";
-
     return (
         <Section black>
             <div className="footer">
@@ -21,22 +17,22 @@ function Footer() {
                     <img src={LOGO_BLACK} alt="YTech NZ logo black"/>
                     <div className="footer__container__row">
                         <div>
-                            <a href={LINKEDINURL} target="_blank">
+                            <a href={Properties.linkedinURL} target="_blank">
                                 <img src={LINKEDIN} alt="LinkedIn logo"/>
                             </a>
                         </div>
                         <div>
-                            <a href={FACEBOOKURL} target="_blank"> 
+                            <a href={Properties.facebookURL} target="_blank"> 
                                 <img src={FACEBOOK} alt="Facebook logo"/>
                             </a>
                         </div>
                         <div>
-                            <a href={INSTAGRAMURL} target="_blank"> 
+                            <a href={Properties.instagramURL} target="_blank"> 
                                 <img src={INSTAGRAM} alt="Instagram logo"/>
                             </a>
                         </div>
                         <div>
-                            <a href={EMAILMAILTO}>
+                            <a href={'mailto:'+ Properties.emailYTech}>
                                 <img src={EMAIL} alt="Email logo"/>
                             </a>
                         </div>
