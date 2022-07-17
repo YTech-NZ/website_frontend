01a5562b9dd7ad1d3b988327d167ed5b66a9885c
mdan066 <mdan066@aucklanduni.ac.nz>
2022-07-17 22:18:51 +1200

    Added comment lines to describe objective of Google signup implementation files and stored them in new folder.

---
+++ src/components/Authentication/google_signup_unfinished/Login.tsx
@@ -0,0 +1,12 @@
+// Script for creating a button for logging into Firebase using Google.
+
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>Login</div>
+
+  )
+}
+
+export default Login
---
+++ src/components/Authentication/google_signup_unfinished/Signup.tsx
@@ -0,0 +1,11 @@
+// Script for creating a button for signing into Firebase using Google.
+
+import React from 'react'
+
+const Signup = () => { 
+    return (
+    <div>Signup</div>
+  )
+}
+
+export default Signup
