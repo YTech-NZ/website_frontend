1c7dfa345f42928a688f9347bd21ede4cac68c20
johnchen383 <john.bm.chen@gmail.com>
2022-07-18 22:08:34 +1200

    fix: resolve ts errors and capitalisation

--- src/components/auth/firebase_hosting/Firebase.tsx
+++ src/components/auth/firebase_hosting/Firebase.tsx
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-import firebaseConfig from "./FirebaseConfig";
+import firebaseConfig from "./firebaseConfig";
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth"; 
--- src/screens/Admin.tsx
+++ src/screens/Admin.tsx
@@ -1,8 +1,8 @@
 import { onAuthStateChanged } from 'firebase/auth';
 import { useState } from 'react'
-import AdminDashboard from '../components/authentication/AdminDashboard';
-import { auth } from '../components/authentication/firebase_hosting/Firebase';
-import SignIn from '../components/authentication/SignIn';
+import AdminDashboard from '../components/auth/AdminDashboard';
+import { auth } from '../components/auth/firebase_hosting/Firebase';
+import SignIn from '../components/auth/SignIn';
 
 function Admin() {
     // this function takes care of sign in status. sign in status (isSignedIn) determines what is shown on /admin page.
@@ -12,7 +12,7 @@ function Admin() {
 
     // This observer gets called whenever the user's sign-in state changes (ie. whenever someone logs in or logs out)
     // See https://firebase.google.com/docs/auth/web/start#set_an_authentication_state_observer_and_get_user_data
-    onAuthStateChanged(auth, (user) => {
+    onAuthStateChanged(auth, (user:any) => {
         if (user){
             setSignInStatus(true);
         } else {
