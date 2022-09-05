d17c38fcc18a135a8708dba74ff640516bdd89b7
lia-arroyo <liayzabel@gmail.com>
2022-09-06 00:47:20 +1200

    feat: added images to values section

--- src/components/Values.tsx
+++ src/components/Values.tsx
@@ -1,6 +1,6 @@
 import '../styles/values.scss'
 import Section from '../widgets/Section'
-import placeHolderImg from '../assets/instagram.png';
+import Image from '../widgets/Image'
 
 function Values() {
   return (
@@ -12,17 +12,17 @@ function Values() {
 
             <div id="pillars">
                 <div className="pillar" id="pink-pillar">
-                    <img src={placeHolderImg}/>
+                    <Image src="https://drive.google.com/file/d/1uMAYwP44W9pNQkc5n0JXfNFn3Oj5Q0Kd/view?usp=sharing"/>
                     <h2>Growth</h2>
                 </div>
 
                 <div className="pillar" id="blue-pillar">
-                    <img src={placeHolderImg}/>
+                    <Image src="https://drive.google.com/file/d/1UkkhT1jQuDLfqecq5DPFHwj3SCBbTinJ/view?usp=sharing" />
                     <h2>Youth Powered</h2>
                 </div>
 
                 <div className="pillar" id="yellow-pillar">
-                    <img src={placeHolderImg}/>
+                    <Image src="https://drive.google.com/file/d/1OwjqnDoiIJL2WSz0Bt4lB98w4xKWDECr/view?usp=sharing" />
                     <h2>Community Focused</h2>
                 </div>
 
