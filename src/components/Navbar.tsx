f9bad1648981acf225a8e017713ab70a2fc234b1
mofumofy <keiranefre@gmail.com>
2023-01-24 21:16:00 +1300

    Feat: all navbar links disabled including logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -52,14 +52,14 @@ function NavBar() {
             <span></span>
             <span></span>
           </div>
-          <a href="/" className='header__logo'>
+          <a role="link" aria-disabled="true" className='header__logo'>
             <img src={phLogo} alt="logo" />
           </a>
           <div className={`header__links hide-for-mobile ${navElementToHighlight}`}>
-            <Link to="/about">About</Link>
-            <Link to="/events">Events</Link>
-            <Link style={{ pointerEvents: 'none' }} to={''}>Community</Link>
-            <Link style={{ pointerEvents: 'none' }} to={''}>Join Us</Link>
+            <Link style={{ pointerEvents: 'none' }} to="/about">About</Link>
+            <Link style={{ pointerEvents: 'none' }} to="/events">Events</Link>
+            <Link style={{ pointerEvents: 'none' }} to="/community">Community</Link>
+            <Link style={{ pointerEvents: 'none' }} to="/joinus">Join Us</Link>
           </div>
         </nav>
         {/* <div className={`header__menu ${handleFades()} has-fade`}>
